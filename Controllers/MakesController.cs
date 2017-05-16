739fa3a0b8d0a45d808ea0225892c3a96269a94d
Robert <chaolong_chen@hotmail.com>
2017-05-16 22:18:16 +0800

    Modify MakesController with Async Method

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApplicationBasic.Core.Models;
 using WebApplicationBasic.Persistence;
 
@@ -16,9 +18,9 @@ namespace WebApplicationBasic.Controllers
         }
 
         [HttpGet("/api/makes")]
-        public IEnumerable<Make> GetMakes() 
+        public async Task<IEnumerable<Make>> GetMakes() 
         {
-            return context.Makes.ToList();
+            return await context.Makes.Include(m => m.Models).ToListAsync();
         } 
     }
 }
