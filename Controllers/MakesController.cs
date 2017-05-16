45990bd904b1c4d9656d7199da21019ad118eb31
Robert <chaolong_chen@hotmail.com>
2017-05-16 22:44:25 +0800

    DI automapper to controller and add createmap with model and resource.

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -5,22 +5,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplicationBasic.Core.Models;
 using WebApplicationBasic.Persistence;
+using AutoMapper;
+using WebApplicationBasic.Controllers.Resources;
 
 namespace WebApplicationBasic.Controllers
 {
     public class MakesController : Controller
     {
         private readonly VegaDbContext context;
+        private readonly IMapper mapper;
 
-        public MakesController(VegaDbContext context)
+        public MakesController(VegaDbContext context, IMapper mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
 
         [HttpGet("/api/makes")]
-        public async Task<IEnumerable<Make>> GetMakes() 
+        public async Task<IEnumerable<MakeResource>> GetMakes() 
         {
-            return await context.Makes.Include(m => m.Models).ToListAsync();
+            // return await context.Makes.Include(m => m.Models).ToListAsync();
+            var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+            return mapper.Map<List<Make>, List<MakeResource>>(makes);
         } 
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,9 +1,15 @@
 using AutoMapper;
+using WebApplicationBasic.Controllers.Resources;
+using WebApplicationBasic.Core.Models;
 
 namespace WebApplicationBasic.Mapping
 {
     public class MappingProfile : Profile
     {
-        
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
     }
 }
